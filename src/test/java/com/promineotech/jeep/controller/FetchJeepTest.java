a0f9a18b7c6637ac16cb81f27718bd07709d7698
Ghide Gebremichael <ghidehab2@gmail.com>
2022-12-20 22:21:17 -0700

    week14-project assignment about Jeep-Sales

---
+++ src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -0,0 +1,67 @@
+package com.promineotech.jeep.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+
+import com.promineotech.jeep.controller.support.FetchJeepTestSupport;
+import com.promineotech.jeep.entity.Jeep;
+import com.promineotech.jeep.entity.JeepModel;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@Sql(
+		scripts = {"classpath:flyway/migrations/V1.0__Jeep_Schema.sql",
+      "classpath:flyway/migrations/V1.1__Jeep_Data.sql"},
+       config = @SqlConfig(encoding = "utf-8"))
+
+class FetchJeepTest extends FetchJeepTestSupport{
+	
+	
+	@Test
+	void testThatJeepAreReturnedWhenAndValidModelAndTrimAreSupplied(){
+
+		//Given: a valid model, trim and URI
+		JeepModel model = JeepModel.WRANGLER;
+		String trim = "Sport";
+		String uri = String.format("%s?model=%s&trim=%s",getBaseUri(),model,trim);
+		
+		
+		//When: a connection is made to the URI
+		ResponseEntity<List<Jeep>> response = getRestTemplate().exchange(uri,HttpMethod.GET,null,
+				new ParameterizedTypeReference<>() {});
+		
+		//Then: a a success(Ok-200) status code is returned
+		
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		
+		//And: the actual list returned is the same as the expected list
+		
+		List<Jeep> expected = buildExpected();
+		System.out.println(expected);
+		assertThat(response.getBody()).isEqualTo(expected);
+	}
+		/*
+		 * Informational responses (100 – 199) 
+		 * Successful responses (200 – 299)
+		 * Redirection messages (300 – 399) 
+		 * Client error responses (400 – 499) 
+		 * Server error responses (500 – 599)
+		 */
+		
+		
+	}
+
+
+
